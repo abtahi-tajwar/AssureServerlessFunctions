e9be223f8ff516280002ae82908adb3fc7b071e1
abtahi-tajwar <abtahitajwar@gmail.com>
2022-06-16 19:10:16 +0600

    initial upload

---
+++ functions/getFeaturedBlogs.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+
+exports.handler = async function(event, context) {
+    const url = `https://api.hubapi.com/cms/v3/blogs/posts?hapikey=\
+    ${process.env.ASSURE_HUBSPOT_API_KEY}\
+    ${event.queryStringParameters.contentGroupId ? `&contentGroupId=${event.queryStringParameters.contentGroupId}`:''}\
+    ${event.queryStringParameters.limit ? `&limit=${event.queryStringParameters.limit}&offset=1` : '' }\    
+    ${event.queryStringParameters.tags ? `&tagId__in=${event.queryStringParameters.tags}` : ''}`
+    console.log(url)
+    const res = await fetch(url)
+    const result = await res.json()
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(result)
+    }
+}
---
+++ functions/getTagIds.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context) {
+    const tags_string = event.queryStringParameters.tags.split(",").map(tag=> tag.split("_").join(" "))
+    let tags = []
+    const res = await fetch(`https://api.hubapi.com/cms/v3/blogs/tags?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}`)
+    const result = await res.json()
+
+    result.results.forEach(item => {
+        if(tags_string.includes(item.name)) {
+            tags.push(item)
+        }
+    })
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(tags)
+    }
+}
