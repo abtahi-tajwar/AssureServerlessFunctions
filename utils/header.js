7bfdac277b24dfac664ec218f6e0be972ca63335
abtahi-tajwar <abtahitajwar@gmail.com>
2022-06-18 13:17:32 +0600

    Content Type updated

--- utils/header.js
+++ utils/header.js
@@ -1,6 +1,7 @@
 const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+    'Content-Type': 'applicaiton/json'
 };
 module.exports = headers
