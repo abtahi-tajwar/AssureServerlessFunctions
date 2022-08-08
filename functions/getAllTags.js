b8a3ffe07cc12846be6efdfd0205fd60f5342bc8
abtahi-tajwar <abtahitajwar@gmail.com>
2022-08-09 01:50:22 +0600

    Get All Tags Function

---
+++ functions/getAllTags.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+const headers = require("../utils/header")
+
+exports.handler = async function(event, context) {
+
+    const url = `https://api.hubapi.com/cms/v3/blogs/tags?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}`
+    const res = await fetch(url)
+    const result = await res.json()
+    return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(result)
+    }
+}
--- functions/searchBlogs.js
+++ functions/searchBlogs.js
@@ -2,7 +2,6 @@ const fetch = require("node-fetch");
 const headers = require("../utils/header")
 
 exports.handler = async function(event, context) {
-
     const url = `https://api.hubspot.com/content/api/v2/blog-posts?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}${event.queryStringParameters.offset ? `&offset=${event.queryStringParameters.offset}` : ''}${event.queryStringParameters.limit ? `&limit=${event.queryStringParameters.limit}` : ''}`
     console.log(url)
     const res = await fetch(url)
