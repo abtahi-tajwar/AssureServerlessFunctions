bd597ee3e88719a733ed133270346685849a228b
abtahi-tajwar <abtahitajwar@gmail.com>
2022-08-09 02:13:20 +0600

    Limit increased for all tags api

--- functions/getAllTags.js
+++ functions/getAllTags.js
@@ -3,7 +3,7 @@ const headers = require("../utils/header")
 
 exports.handler = async function(event, context) {
 
-    const url = `https://api.hubapi.com/cms/v3/blogs/tags?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}`
+    const url = `https://api.hubapi.com/cms/v3/blogs/tags?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}&limit=200`
     const res = await fetch(url)
     const result = await res.json()
     return {
