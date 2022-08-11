b7d903d6a6f09af365a3c1145cfe9c8ed03e345a
abtahi-tajwar <abtahitajwar@gmail.com>
2022-08-11 20:55:37 +0600

    Blog v3 published only

--- functions/getBlogsv3.js
+++ functions/getBlogsv3.js
@@ -8,7 +8,8 @@ exports.handler = async function(event, context) {
     const url = `https://api.hubapi.com/cms/v3/blogs/posts?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}\
 ${contentGroupId ? `&contentGroupId__in=${contentGroupId}` : ""}\
 ${offset ? `&offset=${offset}` : ""}\
-${limit ? `&limit=${limit}` : ""}
+${limit ? `&limit=${limit}` : ""}\
+&state=PUBLISHED
 `
     console.log(url)
     const res = await fetch(url)
