6fdec7be3e6f25289dde25856469b7be90c61c37
abtahi-tajwar <abtahitajwar@gmail.com>
2023-04-03 22:31:35 +0600

    Added search to blog v3

--- functions/getBlogsv3.js
+++ functions/getBlogsv3.js
@@ -6,11 +6,13 @@ exports.handler = async function(event, context) {
     const limit = event.queryStringParameters.limit
     const offset = event.queryStringParameters.offset
     const tags = event.queryStringParameters.tags
+    const search = event.queryStringParameters.search
     const url = `https://api.hubapi.com/cms/v3/blogs/posts?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}\
 ${contentGroupId ? `&contentGroupId__in=${contentGroupId}` : ""}\
 ${offset ? `&offset=${offset}` : ""}\
 ${limit ? `&limit=${limit}` : ""}\
 ${tags ? `&tagId__in=${tags}` : ""}\
+${search ? `&name__icontains=${search}` : ""}\
 &state=PUBLISHED
 `
     console.log(url)
