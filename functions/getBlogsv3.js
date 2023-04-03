5c5a1c4f04b613771d7f11eb3624f5769173f4cb
abtahi-tajwar <abtahitajwar@gmail.com>
2023-04-03 21:21:48 +0600

    Added tags to blogs v3

--- functions/getBlogsv3.js
+++ functions/getBlogsv3.js
@@ -5,10 +5,12 @@ exports.handler = async function(event, context) {
     const contentGroupId = event.queryStringParameters.contentGroupId
     const limit = event.queryStringParameters.limit
     const offset = event.queryStringParameters.offset
+    const tags = event.queryStringParameters.tags
     const url = `https://api.hubapi.com/cms/v3/blogs/posts?hapikey=${process.env.ASSURE_HUBSPOT_API_KEY}\
 ${contentGroupId ? `&contentGroupId__in=${contentGroupId}` : ""}\
 ${offset ? `&offset=${offset}` : ""}\
 ${limit ? `&limit=${limit}` : ""}\
+${tags ? `&tagId__in=${tags}` : ""}\
 &state=PUBLISHED
 `
     console.log(url)
